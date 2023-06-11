45d04df01fe42d306cf902ba9d9c223986423f8e
douglasqs <37385027+douglasqs@users.noreply.github.com>
2023-06-11 15:23:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    // Lógica para enviar o e-mail
+    alert("E-mail enviado com sucesso!");
+});
